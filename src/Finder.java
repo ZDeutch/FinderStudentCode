131f1162b96a44fbc642461d834ca81e800949d0
ZDeutch <zander.deutch@menloschool.org>
2025-11-09 21:35:11 -0800

    Final Version

--- src/Finder.java
+++ src/Finder.java
@@ -13,13 +13,16 @@ import java.io.IOException;
 public class Finder {
 
     private static final String INVALID = "INVALID KEY";
+    private int itemCount;
 
     // Prime number to be an effective mod
     // Around 1 million elements to fit in memory
-    private static final int SIZE = 1000000003;
+    private static int SIZE = 1000003;
 
-    // table to store chained nodes
-    Node[] table = new Node[SIZE];
+    // Arrays which hold the keys for the file and the values for the file respectively
+    String[] keys = new String[SIZE];
+    String[] values = new String[SIZE];
+
 
     public Finder() {
     }
@@ -34,78 +37,118 @@ public class Finder {
             // Read through each item
             String[] parts = line.split(",");
 
-            // Identify the key and value for the item
+            // Create and initialize the keys and values for the given file
             String key = parts[keyCol];
             String value = parts[valCol];
 
-            // Generate a rolling hash value by creating a unique ASCII value based on the key
-            // 31 is what the HashCode method in java does
-            // Also its prime and an odd number to prevent collisions
-            int AASCI = 0;
-            for (int i = 0; i < key.length(); i++) {
-                AASCI = 31 * AASCI + key.charAt(i);
+            // Check if the array is 50% full in which case you resize to make it bigger
+            if (itemCount >= SIZE / 2) {
+                resize();
             }
 
-            // Make sure that fits in your table through modding
-            int index = Math.abs(AASCI % SIZE);
+            // Generate a rolling hash value through Horner's Method
+            long hashVal = hash(key, 0, key.length());
 
-            // Set this as your current node
-            Node current = table[index];
+            // Make sure that fits in your array through modding
+            int index = (int) Math.abs(hashVal % SIZE);
 
-            // And the node you are searching for is initially false
-            boolean found = false;
 
-            // Until there are no more nodes in this chain of table[index]
-            while (current != null) {
-                // See if the keys match for the chained element and the key of the item
-                if (current.getKey().equals(key)) {
-                    // If it matches, then set the item as found
-                    found = true;
-                    break;
-                }
-
-                // Move onto your next node in the table
-                current = current.getNext();
+            // If there is an entry at the given index which is different from the current element
+            // Move your index to the right in search of an empty entry
+            while (keys[index] != null && !keys[index].equals(key)) {
+                index = (index + 1) % SIZE;
             }
 
-            // If you can't find the item in your table
-            if (!found) {
-                // Create a new entry for this item
-                Node newEntry = new Node(key, value, table[index]);
-                // Set the index to the new entry
-                table[index] = newEntry;
+            // Once you find an empty spot in the array you know that it can be filled with an entry
+            if (keys[index] == null) {
+                itemCount++;
             }
+
+            // Move the key and value into the empty spot in the array and move to the next line
+            keys[index] = key;
+            values[index] = value;
             line = br.readLine();
         }
-
-        br.close();
     }
 
-    // Method to find a given item with its UPC Code
+    // Method to find a given item with its key
     public String query(String key) {
 
-        // Generate the rolling hash value
-        int AASCI = 0;
-        for(int i = 0; i < key.length(); i++) {
-            AASCI = AASCI * 31 + key.charAt(i);
-        }
+        // Generate the rolling hash value using Horner's Method
+        long hasVal = hash(key, 0, key.length());
 
-        // Set the first node to the top of the chain in the table
-        Node temp = table[Math.abs(AASCI % SIZE)];
+        // Create its array index by modding
+        int index = (int) Math.abs(hasVal % SIZE);
 
-        // Until you reach the end of the chail
-        while(temp != null) {
+        // Use a starting index to ensure you don't wrap around the whole array
+        int startIndex = index;
 
-            // If your key is equal to the node's key
-            if(key.equals(temp.getKey())) {
-                // Return its value
-                return temp.getValue();
-            } else {
-                // Otherwise move down the chain
-                temp = temp.getNext();
+        // While the element is not empty
+        // Check if they are equal and return the match
+        while (keys[index] != null) {
+            if (keys[index].equals(key)) {
+                return values[index];
+            }
+            // Otherwise keep moving to the right until you find the correct element
+            index = (index + 1) % SIZE;
+
+            // If you loop around or don't get a hit then it was an invalid search
+            if (index == startIndex) {
+                return INVALID;
             }
         }
-
         return INVALID;
+    }
+
+    // Horner's Method
+    public static long hash(String sequence, int start, int end) {
+        long hash = 0;
+        for (int i = start; i < end; i++) {
+            // Use extended ASCII as test files have numerous types of characters
+            hash = (hash * 256 + sequence.charAt(i));
+        }
+        return hash;
+    }
+
+    // Method to resize arrays after 50% capacity
+    public void resize() {
+        // Save old size and reset item count
+        int oldSize = SIZE;
+        itemCount = 0;
+
+        // Double the size of your new arrays
+        SIZE *= 2;
+
+        // Store old arrays and create expanded arrays
+        String[] oldKeys = keys;
+        String[] oldValues = values;
+
+        keys = new String[SIZE];
+        values = new String[SIZE];
+
+        // For every element in your old array
+        for (int i = 0; i < oldSize; i++) {
+            // If that index wasn't null
+            if (oldKeys[i] != null) {
+                // Rehash the element with the updated size
+                long hashVal = hash(oldKeys[i], 0, oldKeys[i].length());
+
+                // Make the index using mod
+                int index = (int) Math.abs(hashVal % SIZE);
+
+                // increment your index if the current position is filled
+                while (keys[index] != null) {
+                    // Make sure you are still in bounds of the array
+                    index = (index + 1) % SIZE;
+                }
+
+                // Now update the new array with the old array's contents
+                keys[index] = oldKeys[i];
+                values[index] = oldValues[i];
+
+                // Indicate that a new item has been added
+                itemCount++;
+            }
+        }
     }
 }
